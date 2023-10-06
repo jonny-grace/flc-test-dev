7a73a394fc4be1169513a375296c5c51764f08cd
DAdawit <58243647+DAdawit@users.noreply.github.com>
2023-10-06 07:40:50 +0300

    scroller button

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,8 @@ import "react-tabs/style/react-tabs.css";
 import { Inter } from "next/font/google";
 
 import IntegratedTabContent from "./IntegratedTabContent";
+import { LeftArrow } from "../../assets/Icons/LeftArrow";
+import { RightArrow } from "../../assets/Icons/RightArrow";
 const inter = Inter({
   subsets: ["cyrillic"],
   weight: ["400"],
@@ -14,12 +16,44 @@ const interBold = Inter({
 });
 
 const GaleryTab = ({ caseStudy }) => {
-  const [tabs, setTab] = useState("integrated");
+  const tabsItems = [
+    "Integrated",
+    "FMCG",
+    "Electronics",
+    "Fashn & Lifestyle",
+    "Food & Beverage",
+  ];
+
+  const [tabs, setTab] = useState("Integrated");
 
   const [activeTab, setActiveTab] = useState(0);
 
   const handleTabClick = (index) => {
     setActiveTab(index);
+  };
+
+  const handleLeft = (tab) => {
+    let indexOfCurrentTab = tabsItems.indexOf("tab");
+    for (let i = 0; i < tabsItems.length; i++) {
+      if (tabsItems[i] === tab) {
+        if (i === 0) {
+          setTab(tabsItems[i]);
+        } else {
+          setTab(tabsItems[i - 1]);
+        }
+      }
+    }
+  };
+  const handleRight = (tab) => {
+    for (let i = 0; i < tabsItems.length; i++) {
+      if (tabsItems[i] === tab) {
+        if (i === tabsItems.length - 1) {
+          setTab(tabsItems[i]);
+        } else {
+          setTab(tabsItems[i + 1]);
+        }
+      }
+    }
   };
 
   return (
@@ -81,12 +115,12 @@ const GaleryTab = ({ caseStudy }) => {
       <div className="w-full mx-auto ">
         <div className="flex justify-evenly items-center gap-2 flex-wrap max-w-2xl mx-auto">
           <button
-            onClick={() => setTab("integrated")}
+            onClick={() => setTab("Integrated")}
             className="flex md:items-center md:justify-center flex-col md:flex-row gap-2"
           >
             <span
               className={
-                tabs === "integrated"
+                tabs === "Integrated"
                   ? `${inter.interBold} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-600 whitespace-nowrap`
                   : `${inter.className} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-500 whitespace-nowrap`
               }
@@ -128,12 +162,12 @@ const GaleryTab = ({ caseStudy }) => {
           <button>|</button>
 
           <button
-            onClick={() => setTab("Fash")}
+            onClick={() => setTab("Fashn & Lifestyle")}
             className="flex md:items-center md:justify-center flex-col md:flex-row gap-2"
           >
             <span
               className={
-                tabs === "Fash"
+                tabs === "Fashn & Lifestyle"
                   ? `${inter.interBold} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-600 whitespace-nowrap`
                   : `${inter.className} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-500 whitespace-nowrap`
               }
@@ -143,12 +177,12 @@ const GaleryTab = ({ caseStudy }) => {
           </button>
           <button>|</button>
           <button
-            onClick={() => setTab("food")}
+            onClick={() => setTab("Food & Beverage")}
             className="flex md:items-center md:justify-center flex-col md:flex-row gap-2"
           >
             <span
               className={
-                tabs === "food"
+                tabs === "Food & Beverage"
                   ? `${inter.interBold} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-600 whitespace-nowrap`
                   : `${inter.className} font-bold text-sm md:text-lg xxl:text-[22px] text-gray-500 whitespace-nowrap`
               }
@@ -158,13 +192,35 @@ const GaleryTab = ({ caseStudy }) => {
           </button>
         </div>
       </div>
-      <section className="w-full mt-8">
-        <div>{tabs === "integrated" ? <IntegratedTabContent /> : null}</div>
+      <section className="w-full mt-8 min-h-screen">
+        <div>{tabs === "Integrated" ? <IntegratedTabContent /> : null}</div>
         <div>{tabs === "FMCG" ? <IntegratedTabContent /> : null}</div>
         <div>{tabs === "Electronics" ? <IntegratedTabContent /> : null}</div>
-        <div>{tabs === "Fash" ? <IntegratedTabContent /> : null}</div>
-        <div>{tabs === "food" ? <IntegratedTabContent /> : null}</div>
+        <div>
+          {tabs === "Fashn & Lifestyle" ? <IntegratedTabContent /> : null}
+        </div>
+        <div>
+          {tabs === "Food & Beverage" ? <IntegratedTabContent /> : null}
+        </div>
       </section>
+      <div className="flex justify-end gap-3 mt-5">
+        <button
+          className="flex justify-center items-center h-12 w-12 border-[1px] rounded-full hover:border-2 hover:border-gray-600"
+          onClick={() => handleLeft(tabs)}
+        >
+          <span className="text-gray-600">
+            <LeftArrow />
+          </span>
+        </button>
+        <button
+          className="flex justify-center items-center h-12 w-12 border-[1px] rounded-full hover:border-2 hover:border-gray-600"
+          onClick={() => handleRight(tabs)}
+        >
+          <span className="text-gray-600">
+            <RightArrow />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/pages/hello/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-5xl mx-auto">
+        <div class="grid grid-rows-6 grid-cols-3  grid-flow-col gap-4">
+          <div class="row-start-1 row-end-3 col-start-1 col-end-3   h-36 bg-pink-300">
+            01
+          </div>
+          {[0, 1, 2].map((item, index) => (
+            <div
+              key={index}
+              className={`row-start-${index + 1} bg-pink-400 col-start-3`}
+            >
+              02
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
