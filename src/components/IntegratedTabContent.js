74654696d606ecb2371bda7dffbf53c23e6df0b6
jonny-grace <johnlemma9@gmail.com>
2023-10-17 09:06:49 +0300

    footer and galery image problem fixed

--- src/components/IntegratedTabContent.js
+++ src/components/IntegratedTabContent.js
@@ -9,8 +9,8 @@ import Link from "next/link";
 const BoxComponent = () => {
   return (
     // general box
-    <div className=" w-full bg-green-50">
-      <div className="   md:w-[100%]  bg-gray-500 md:h-[670px] font-inter">
+    <div className=" w-full">
+      <div className="   xxl:mx-48 bg-gray-500 md:h-[670px] font-inter">
         <div className=" flex flex-col md:flex-row h-full">
           <div className=" md:w-[40%] bg-blue-300 h-full">
             <div className="relative lg:w-[500px] xl:w-[610px] md:w-[450px]  xxl:w-[751.36px] flex h-full">
@@ -30,7 +30,7 @@ const BoxComponent = () => {
                   <h2 className="text-4xl xl:w-[450px] xxl:w-[566px] xxl:text-[65px] font-bold mt-7 px-4">
                     Honor <br/> Magic Pro Launch
                   </h2>
-                  <p className="mt-6 xxl:w-[596px] xxl:text-[18px] md:w-[380px] font-small  opacity-75 text-sm ml-5  mr-10">
+                  <p className="mt-6 xxl:w-[296px] xxl:text-[18px] md:w-[380px] font-small  opacity-75 text-sm ml-5  mr-10">
                     Duis autem vel eum iriure dolor in hendrerit in vulputate
                     velit esse molestie consequat, vel illum dolore eu feugiat
                     nulla
